86cd23b02f05038e1c7491f9725012b32f20b1a3
atlassanjay <arrowzflame321@gmail.com>
2019-10-19 15:55:00 +0100

    Update Program.cs

--- MCrypt/Program.cs
+++ MCrypt/Program.cs
@@ -26,8 +26,6 @@ namespace MCrypt
         //TODO: arg[1]? = Runtime https://docs.microsoft.com/en-us/dotnet/core/rid-catalog If self contained
         public static void Main(string[] args)
         {
-            args = new string[1];
-            args[0] = @"C:\users\j\downloads\Obfuscar.Console.exe";
             if (args.Length <= 0)
             {
                 Console.WriteLine("Please specify a file.");
